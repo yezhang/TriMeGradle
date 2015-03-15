32720efaa3bba4af449e5c87986249ab24fc8713
littledeer <zhlyxhygl@126.com>
2015-03-15 20:56:02 +0800

    order
    create order

---
+++ src/main/java/trime/controller/OrderController.java
@@ -0,0 +1,15 @@
+package trime.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Created by zhanglu on 2015/3/15.
+ */
+@Controller
+public class OrderController {
+
+    public ModelAndView createOrder(){
+
+    }
+}
